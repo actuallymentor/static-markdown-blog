39c3bfd500ffffac9dd4258d1f8054a75894fcc9
Mentor <mentor@palokaj.co>
2016-12-19 13:29:47 +0100

    Pug blog file

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 // Get md and fs managers
 const md = require( __dirname + '/system/modules/compilemd' )
+const pug = require( __dirname + '/system/modules/compilepug' )
+
 const fs = require( 'fs' )
 const ncp = require( 'ncp' )
 
 md( __dirname + '/content/post.md' ).then( markdown => {
-	fs.writeFile( __dirname + '/public/post.html', markdown, err => {
-		if ( err ) throw err
-	} )
+	let template = __dirname + '/system/templates/blog.pug'
+	let target = __dirname + '/public/post.html'
+	pug( template, { content: markdown }, target )
 } )
 
 // Copy all assets from source
---
+++ system/modules/compilepug.js
@@ -0,0 +1,14 @@
+const pug = require( 'pug' )
+const fs = require( 'fs' )
+
+let render = ( pugfile, locals, target ) => {
+	return new Promise( ( resolve, reject ) => {
+		let pugresult = pug.renderFile( pugfile, locals )
+		fs.writeFile( target, pugresult, err => {
+			if ( err ) reject( err )
+			resolve( )
+		} )
+	} )
+}
+
+module.exports = render
