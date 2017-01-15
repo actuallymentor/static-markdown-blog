e067cec7578012698e697e57955480d6d4a7db4a
Mentor <mentor@palokaj.co>
2017-01-15 23:49:58 +0000

    Live url

--- system/modules/config.js
+++ system/modules/config.js
@@ -19,7 +19,7 @@ let config = {
 		content: path.normalize( __dirname + '/../../content/' ),
 		public: path.normalize( __dirname + '/../../docs/' ),
 		templates: path.normalize( __dirname + '/../templates/' ),
-		url: process.env.local ? "http://localhost:3000/" : "https://www.yourliveblog.com",
+		url: process.env.local ? "http://localhost:3000/" : "https://actuallymentor.github.io/static-markdown-blog/",
 		year: new Date().getFullYear(),
 		today: today,
 		// Google verification code
