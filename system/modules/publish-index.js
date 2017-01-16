ba4c83a41536efcf0c846ad7e73952d506a5c024
Mentor <mentor@palokaj.co>
2017-01-16 16:34:46 +0100

    Promisify more

--- system/modules/publish-index.js
+++ system/modules/publish-index.js
@@ -4,27 +4,29 @@
 const pug = require( __dirname + '/parse-pug' )
 const write = require( __dirname + '/publish-file' )
 const fs = require( 'fs' )
+const pfs = require( __dirname + '/parse-fs' )
 
 module.exports = ( site, allposts ) => {
 	// Make public folder if it does not exist
-	if( !fs.existsSync( site.system.public ) ) fs.mkdirSync( site.system.public )
-	return new Promise( ( resolve, reject ) => {
-		// Generate page
-		pug( site.system.templates + 'index.pug', {
-			site: site,
-			posts: allposts,
-			url: site.system.url,
-			// Meta structure
-			file: {
-				meta: {
-					title: site.identity.title,
-					desc: site.identity.desc,
-					featuredimg: site.identity.image,
-					published: site.system.today
+	pfs.mkdir( site.system.public ).then( f => {
+		return new Promise( ( resolve, reject ) => {
+			// Generate page
+			pug( site.system.templates + 'index.pug', {
+				site: site,
+				posts: allposts,
+				url: site.system.url,
+				// Meta structure
+				file: {
+					meta: {
+						title: site.identity.title,
+						desc: site.identity.desc,
+						featuredimg: site.identity.image,
+						published: site.system.today
+					}
 				}
-			}
-		} ).then( page => {
-			write( site.system.public + 'index.html', page ).then( resolve )
+			} ).then( page => {
+				return write( site.system.public + 'index.html', page )
+			} ).then( resolve ).catch( reject )
 		} )
 	} )
 }
